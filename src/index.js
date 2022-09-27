3cc0a3b7aa29540a3563a82fabdf339d0389ae7f
ndorrh <ndorrh@gmail.com>
2022-09-27 18:21:26 +0100

    Add index.js in src/ with array of objects that contain to do task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-import _ from 'lodash';
+const toDoContainer = document.getElementById('container');
 
-function component() {
-  const element = document.createElement('div');
+const Activities = [
+  {
+    description: 'clean the house',
+    completed: true,
+    index: 0,
+  },
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  {
+    description: 'go to school',
+    completed: false,
+    index: 1,
+  },
 
-  return element;
+  {
+    description: 'Watch tv',
+    completed: true,
+    index: 3,
+  },
+
+  {
+    description: 'Go swimming',
+    completed: false,
+    index: 4,
+  },
+
+  {
+    description: 'Go swimming',
+    completed: false,
+    index: 4,
+  },
+
+  {
+    description: 'Go swimming',
+    completed: false,
+    index: 4,
+  },
+]
+
+const show = (arr) => {
+  let str = '';
+  arr.forEach((e) => {
+    toDoContainer.innerHTML += `<div class="activity-container"><input type="checkbox"><li class ="item">${arr[arr.indexOf(e)].description}</li><i class="fa-solid fa-ellipsis-vertical"></i></div>`
+  })
 }
 
-document.body.appendChild(component());
+show(Activities);
